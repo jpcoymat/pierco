9e9f446797e99c69beb00053410c1ebe14ab8861
Jean Pierre Coymat <jpcoymat@gmail.com>
2011-10-29 21:28:37 +0200

    new file:   db/migrate/20111029190316_company_fields.rb 	new file:   db/migrate/20111029190750_rename_about_us_fields.rb

---
+++ db/migrate/20111029190316_company_fields.rb
@@ -0,0 +1,12 @@
+class CompanyFields < ActiveRecord::Migration
+  def self.up
+    add_column :companies, :slogan, :string, :limit => 150
+    add_column :companies, :about_us_side_1, :text
+    add_column :companies, :about_us_side_2, :text
+    add_column :companies, :about_us_side_3, :text
+    rename_column :companies, :about_us, :about_us_main
+  end
+
+  def self.down
+  end
+end
---
+++ db/migrate/20111029190750_rename_about_us_fields.rb
@@ -0,0 +1,15 @@
+class RenameAboutUsFields < ActiveRecord::Migration
+
+  def self.up
+    rename_column :companies, :about_us_side_1, :headline_1_content
+    rename_column :companies, :about_us_side_2, :headline_2_content
+    rename_column :companies, :about_us_side_3, :headline_3_content
+    add_column :companies, :headline_1_title, :string, :limit => 50
+    add_column :companies, :headline_2_title, :string, :limit => 50
+    add_column :companies, :headline_3_title, :string, :limit => 50
+    
+  end
+
+  def self.down
+  end
+end
