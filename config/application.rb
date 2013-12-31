99dd98bfd1ba07f23b5b26b38ce1eced37aa7010
Jean Pierre Coymat <jpcoymat@gmail.com>
2013-12-31 00:04:28 +0000

    commit

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module Pierco
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    # Enable the asset pipeline
+    config.assets.enabled = false
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
