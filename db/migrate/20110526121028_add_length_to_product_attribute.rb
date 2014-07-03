216292bbca82390ea7443a14b8fc2719cb44cad3
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-07-03 06:12:50 +0000

    changes

--- db/migrate/20110526121028_add_length_to_product_attribute.rb
+++
@@ -1,9 +0,0 @@
-class AddLengthToProductAttribute < ActiveRecord::Migration
-
-  def self.up
-    add_column :product_attributes, :length, :decimal, :precision => 9, :scale => 2
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20110527204454_drop_table_product_attributes.rb
+++
@@ -1,9 +0,0 @@
-class DropTableProductAttributes < ActiveRecord::Migration
-
-  def self.up
-    drop_table :product_attributes
-  end
-
-  def self.down
-  end
-end
