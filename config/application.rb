79f0ae73c80a45681890c85cb2d0ed96c2556c07
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-07-04 09:52:30 +0000

    changes

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,24 @@ module Pierco
     config.assets.enabled = true
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
+
+    config.serve_static_assets = true
+
   end
 end
