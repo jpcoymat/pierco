ccb772c06077abc2afd3aa51fd43aa858932dd38
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-12-31 17:42:16 -0500

    changes

---
+++ db/migrate/20141231221439_drop_image_filename.rb
@@ -0,0 +1,9 @@
+class DropImageFilename < ActiveRecord::Migration
+  def change
+    remove_column :products, :image_filename
+    remove_column :suppliers, :image_filename
+    remove_column :companies, :image_filename
+    add_column :suppliers, :supplier_picture, :string
+    add_column :companies, :company_picture, :string
+  end
+end
