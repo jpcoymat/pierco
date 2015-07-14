5841af4ec95d13eaf80d2e4d3e8d1cdf070cc389
Jean Pierre Coymat <jpcoymat@gmail.com>
2015-07-14 21:33:59 +0000

    changes

--- app/controllers/admin/product_photos_controller.rb
+++ app/controllers/admin/product_photos_controller.rb
@@ -1,22 +1,54 @@
 class Admin::ProductPhotosController < ApplicationController
+
+  layout 'admin'
+  before_filter :authorize
+
+  before_action :set_product_photo, only: [:show, :edit, :update, :destroy]
+  before_action :set_produdct
+
   def index
+    @product_photos = @product.product_photos
   end
 
   def show
+  
   end
 
   def new
+    @product_photo = ProductPhoto.new
   end
 
   def create
+    @product_photo = ProductPhot.new(product_photo_parms)
+    if @product_photo.save
+    end
   end
 
   def edit
+  
   end
 
   def update
+    if @product_photo.update_attributes(product_photo_parms)
+    end
   end
 
   def destroy
+    @product_photo.destroy
   end
+
+  private
+
+    def set_product_photo
+      @product_photo = ProductPhoto.find(params[:id])
+    end
+
+    def set_product
+      @product = Product.find(params[:product_id])
+    end
+
+    def product_photo_parms
+      params.require(:product_photo).permit()
+    end
+
 end
