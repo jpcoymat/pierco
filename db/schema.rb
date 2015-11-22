409c4ac3e36762c6e320aad0fe2982be7dfc40ca
Jean Pierre Coymat <jpcoymat@gmail.com>
2015-11-22 20:50:52 +0000

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,21 @@ ActiveRecord::Schema.define(version: 20150107230110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "change_logs", force: :cascade do |t|
+  create_table "change_logs", force: true do |t|
     t.integer  "reference_object_id"
-    t.string   "reference_object_type"
-    t.string   "reference_attribute"
-    t.string   "old_value"
-    t.string   "new_value"
+    t.string   "reference_object_type", limit: nil
+    t.string   "reference_attribute",   limit: nil
+    t.string   "old_value",             limit: nil
+    t.string   "new_value",             limit: nil
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
   end
 
-  create_table "companies", force: :cascade do |t|
+  create_table "companies", force: true do |t|
     t.string   "name",                   limit: 100,                          null: false
     t.string   "address_1",              limit: 100,                          null: false
-    t.string   "address_2"
+    t.string   "address_2",              limit: nil
     t.string   "city",                   limit: 100,                          null: false
     t.string   "state",                  limit: 50,                           null: false
     t.string   "postal_code",            limit: 10,                           null: false
@@ -55,10 +55,10 @@ ActiveRecord::Schema.define(version: 20150107230110) do
     t.string   "headline_1_title",       limit: 50
     t.string   "headline_2_title",       limit: 50
     t.string   "headline_3_title",       limit: 50
-    t.string   "company_picture"
+    t.string   "company_picture",        limit: nil
   end
 
-  create_table "contacts", force: :cascade do |t|
+  create_table "contacts", force: true do |t|
     t.integer  "company_id",            null: false
     t.string   "name",       limit: 75, null: false
     t.string   "email",      limit: 75, null: false
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20150107230110) do
     t.datetime "updated_at"
   end
 
-  create_table "distributors", force: :cascade do |t|
+  create_table "distributors", force: true do |t|
     t.integer  "company_id",                                       null: false
     t.string   "name",        limit: 100,                          null: false
     t.string   "address_1",   limit: 100,                          null: false
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 20150107230110) do
     t.decimal  "longitude",               precision: 10, scale: 5
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", force: true do |t|
     t.string   "title",      limit: 100, null: false
     t.text     "content",                null: false
     t.integer  "company_id",             null: false
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 20150107230110) do
     t.datetime "updated_at"
   end
 
-  create_table "product_categories", force: :cascade do |t|
+  create_table "product_categories", force: true do |t|
     t.integer  "company_id",                                      null: false
     t.string   "name",                limit: 50,                  null: false
     t.string   "description",         limit: 100,                 null: false
@@ -117,17 +117,17 @@ ActiveRecord::Schema.define(version: 20150107230110) do
     t.boolean  "x_factor",                        default: false
   end
 
-  create_table "product_dimensions", force: :cascade do |t|
+  create_table "product_dimensions", force: true do |t|
     t.integer  "product_id"
-    t.string   "name",                                                    null: false
-    t.decimal  "length",                         precision: 10, scale: 2
-    t.decimal  "width",                          precision: 10, scale: 2
-    t.decimal  "maximum_pressure",               precision: 10, scale: 2
-    t.decimal  "maximum_weight",                 precision: 10, scale: 2
+    t.string   "name",                limit: nil,                          null: false
+    t.decimal  "length",                          precision: 10, scale: 2
+    t.decimal  "width",                           precision: 10, scale: 2
+    t.decimal  "maximum_pressure",                precision: 10, scale: 2
+    t.decimal  "maximum_weight",                  precision: 10, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "color",               limit: 50
-    t.decimal  "diameter",                       precision: 10, scale: 2
+    t.decimal  "diameter",                        precision: 10, scale: 2
     t.string   "part_number",         limit: 75
     t.string   "service_description", limit: 20
     t.string   "lateral_wall",        limit: 20
@@ -139,14 +139,14 @@ ActiveRecord::Schema.define(version: 20150107230110) do
     t.string   "x_factor",            limit: 50
   end
 
-  create_table "product_photos", force: :cascade do |t|
+  create_table "product_photos", force: true do |t|
     t.integer  "product_id"
-    t.string   "product_photo"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "product_photo", limit: nil
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table "product_subcategories", force: :cascade do |t|
+  create_table "product_subcategories", force: true do |t|
     t.integer  "product_category_id",                         null: false
     t.string   "name",                limit: 50,              null: false
     t.string   "description",         limit: 100,             null: false
@@ -155,60 +155,60 @@ ActiveRecord::Schema.define(version: 20150107230110) do
     t.integer  "display_priority",                default: 0
   end
 
-  create_table "products", force: :cascade do |t|
-    t.integer  "supplier_id",                                      null: false
-    t.string   "name",                   limit: 50,                null: false
-    t.boolean  "active",                            default: true, null: false
+  create_table "products", force: true do |t|
+    t.integer  "supplier_id",                                       null: false
+    t.string   "name",                   limit: 50,                 null: false
+    t.boolean  "active",                             default: true, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "product_category_id"
     t.integer  "product_subcategory_id"
     t.text     "description"
-    t.string   "product_picture"
+    t.string   "product_picture",        limit: nil
   end
 
-  create_table "supplier_contacts", force: :cascade do |t|
-    t.integer  "supplier_id",            null: false
-    t.string   "first_name",  limit: 50, null: false
-    t.string   "last_name",   limit: 50, null: false
-    t.string   "position"
-    t.string   "telephone"
-    t.string   "cell_phone"
-    t.string   "email"
-    t.string   "fax"
-    t.string   "address_1"
-    t.string   "address_2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "postal_code"
-    t.string   "country"
+  create_table "supplier_contacts", force: true do |t|
+    t.integer  "supplier_id",             null: false
+    t.string   "first_name",  limit: 50,  null: false
+    t.string   "last_name",   limit: 50,  null: false
+    t.string   "position",    limit: nil
+    t.string   "telephone",   limit: nil
+    t.string   "cell_phone",  limit: nil
+    t.string   "email",       limit: nil
+    t.string   "fax",         limit: nil
+    t.string   "address_1",   limit: nil
+    t.string   "address_2",   limit: nil
+    t.string   "city",        limit: nil
+    t.string   "state",       limit: nil
+    t.string   "postal_code", limit: nil
+    t.string   "country",     limit: nil
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "suppliers", force: :cascade do |t|
+  create_table "suppliers", force: true do |t|
     t.string   "name",             limit: 100, null: false
-    t.string   "address_1"
-    t.string   "address_2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "postal_code"
-    t.string   "country"
+    t.string   "address_1",        limit: nil
+    t.string   "address_2",        limit: nil
+    t.string   "city",             limit: nil
+    t.string   "state",            limit: nil
+    t.string   "postal_code",      limit: nil
+    t.string   "country",          limit: nil
     t.integer  "company_id",                   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "website",          limit: 100
     t.string   "telephone",        limit: 20
-    t.string   "supplier_picture"
+    t.string   "supplier_picture", limit: nil
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name",      limit: 50, null: false
-    t.string   "last_name",       limit: 50, null: false
-    t.string   "hashed_password",            null: false
-    t.string   "username",        limit: 25, null: false
+  create_table "users", force: true do |t|
+    t.string   "first_name",      limit: 50,  null: false
+    t.string   "last_name",       limit: 50,  null: false
+    t.string   "hashed_password", limit: nil, null: false
+    t.string   "username",        limit: 25,  null: false
     t.string   "email",           limit: 50
-    t.integer  "company_id",                 null: false
+    t.integer  "company_id",                  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
