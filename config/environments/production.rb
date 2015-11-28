b38cc13b509185a9a2e66fd57e49c6dcfdeba7b8
Jean Pierre Coymat <jpcoymat@gmail.com>
2015-11-28 09:46:53 -0800

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Pierco::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_files = true
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
