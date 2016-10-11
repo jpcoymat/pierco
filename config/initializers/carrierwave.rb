cd724613209981f4b5eb4e79ad6e7212c639db08
Jean Pierre Coymat <jpcoymat@gmail.com>
2016-10-11 14:27:05 -0400

    add secrets file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     aws_access_key_id: Rails.application.secrets.aws_access_key,        # required
     aws_secret_access_key: Rails.application.secrets.aws_secret_key  # required
   }
-  config.fog_directory  = Rails.application.secrets.aws_bucket                 # required
+  config.fog_directory  = Rails.application.secrets.aws_buckete                 # required
 end
