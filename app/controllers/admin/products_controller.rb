81082b8b7f2c8d00418d2875bfab08fa63644d8e
Jean Pierre Coymat <jpcoymat@gmail.com>
2019-02-04 23:46:26 -0500

    product page improvements

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
   layout 'admin'
+  
+  before_action :set_product, only: [:show, :edit, :update]
 
   def lookup 
     set_filter_criteria
@@ -8,44 +10,65 @@ class Admin::ProductsController < ApplicationController
   end
   
   def show
-    @product = Product.find(params[:id])
   end
   
   def new
     @product = Product.new
     set_filter_criteria
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
   def create
     @product = Product.new(params[:product])
-    if @product.save        
-      flash[:alert] = nil
-      flash[:notice] = "Producto creado exitosamnte"
-      render action: 'show'
-    else
-      flash[:notice] = nil
-      flash[:alert] = "Error creando producto"
-      set_filter_criteria
-      render action: 'new'
+    respond_to do |format|
+      if @product.save     
+        format.html do
+          flash[:alert] = nil
+          flash[:notice] = "Producto creado exitosamnte"
+          render action: 'show'
+        end   
+        format.js
+      else
+        format.html do
+          flash[:notice] = nil
+          flash[:alert] = "Error creando producto"
+          set_filter_criteria
+          render action: 'new'          
+        end
+        format.js
+      end      
     end
   end
   
   def edit
-    @product = Product.find(params[:id])
     set_filter_criteria
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
   def update
-    @product = Product.find(params[:id])
-    if @product.update_attributes(params[:product]) 
-      flash[:alert] = nil
-      flash[:notice] = "Producto actualizado exitosamente"
-      render action: 'show'
-    else
-      flash[:notice] = nil
-      flash[:alert] = "Error actualizando producto"
-      set_filter_criteria
-      render action: 'edit'    
+    respond_to do |format|
+      if @product.update_attributes(params[:product]) 
+        format.html do
+          flash[:alert] = nil
+          flash[:notice] = "Producto actualizado exitosamente"
+          render action: 'show'                    
+        end
+        format.js
+      else
+        format.html do
+          flash[:notice] = nil
+          flash[:alert] = "Error actualizando producto"
+          set_filter_criteria
+          render action: 'edit'    
+        end
+        format.js
+      end      
     end
   end
   
@@ -73,6 +96,10 @@ class Admin::ProductsController < ApplicationController
     @suppliers = @company.suppliers
     @product_categories = @company.product_categories
     @product_subcategories = @company.product_subcategories    
+  end
+  
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 
